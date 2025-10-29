95d556f724a130525a0e124698113c3523aa4747
MinecraftPEayer <87757022+MinecraftPEayer@users.noreply.github.com>
2025-10-29 14:34:48 +0000

    fix: fix missing-suspense-with-csr-bailout error on build

---
+++ src/app/auth/discord/components/main.tsx
@@ -0,0 +1,47 @@
+import { ThemeProvider } from '@emotion/react';
+import { createTheme, CssBaseline } from '@mui/material';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+const Main = () => {
+    const params = useSearchParams();
+
+    useEffect(() => {
+        const code = params.get('code');
+        const authenticate = async () => {
+            const resp = await axios.post('/api/auth/discord', { code });
+            const body = resp.data;
+            if (body.success) {
+                const { token } = body;
+                document.cookie = `token=${token}; path=/; max-age=604800`;
+                window.location.href = '/';
+            }
+        };
+        if (!code) {
+            window.location.href =
+                process.env.NEXT_PUBLIC_DISCORD_AUTHORIZE_URL || '';
+        } else {
+            authenticate();
+        }
+    }, []);
+
+    return (
+        <div>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+                <p className="text-md text-gray-400">
+                    Please wait while we are logging you in...
+                </p>
+            </ThemeProvider>
+        </div>
+    );
+};
+
+export default Main;
--- src/app/auth/discord/page.tsx
+++ src/app/auth/discord/page.tsx
@@ -1,46 +1,14 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
-import axios from 'axios';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
-});
+import { Suspense } from 'react';
+import Main from './components/main';
 
 const Page = () => {
-    const params = useSearchParams();
-
-    useEffect(() => {
-        const code = params.get('code');
-        const authenticate = async () => {
-            const resp = await axios.post('/api/auth/discord', { code });
-            const body = resp.data;
-            if (body.success) {
-                const { token } = body;
-                document.cookie = `token=${token}; path=/; max-age=604800`;
-                window.location.href = '/';
-            }
-        };
-        if (!code) {
-            window.location.href =
-                process.env.NEXT_PUBLIC_DISCORD_AUTHORIZE_URL || '';
-        } else {
-            authenticate();
-        }
-    }, []);
-
     return (
         <div className="h-full flex justify-center content-center flex-wrap">
-            <ThemeProvider theme={darkTheme}>
-                <CssBaseline />
-                <p className="text-md text-gray-400">
-                    Please wait while we are logging you in...
-                </p>
-            </ThemeProvider>
+            <Suspense>
+                <Main />
+            </Suspense>
         </div>
     );
 };
